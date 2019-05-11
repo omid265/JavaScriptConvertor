3db11911d97ced815d4dee151927cca878914688
omid265 <omid_265@ymail.com>
2019-05-11 12:50:18 +0430

    Versoin 0.0.1
    this is the first version
    in this version you can convert JSON Object to QueryString
    You Can Convert Persian and arabic Number to English number

---
+++ JavaScriptConvertor.js
@@ -0,0 +1,31 @@
+
+var Convert = {
+    Objects: {
+        ToQueryString: function (v) {
+            return Object.keys(v).map(function (k) {
+                return encodeURIComponent(k) + '=' + encodeURIComponent(v[k])
+            }).join('&')
+        },
+    },
+    Numbert: {
+        parseArabic: function (str) {
+            return Number(str.replace(/[٠١٢٣٤٥٦٧٨٩]/g, function (d) {
+                return d.charCodeAt(0) - 1632; // Convert Arabic numbers
+            }).replace(/[۰۱۲۳۴۵۶۷۸۹]/g, function (d) {
+                return d.charCodeAt(0) - 1776; // Convert Persian numbers
+            }));
+        },
+        ToPersianNumber(pNumber) {
+            var _self = this;
+            var mySeperatedDate = (pNumber).split('/')
+
+            var result = "";
+            $.each(mySeperatedDate, function (i, v) {
+                v = _self.parseArabic(v);
+                result += (v < 10 ? ("" + 0 + v) : v) + "/";
+            })
+            result = result.slice(0, -1)
+            return result;
+        }
+    }
+}
